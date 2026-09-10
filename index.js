fd8778635a9b3be298602367ef7557677dba8109
unknown <deniskalol227228@yandex.ru>
2026-09-10 08:43:00 +0300

    Added student info and PI calculations

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http'); 
+const p=(16*Math.atan(1/5)-4*Math.atan(1/239)).toFixed(16)
 const server = http.createServer((req, res) => { 
 res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); 
-res.end('<h1>Привет, мир!</h1>'); 
+res.end('<h1>Овчинников Денис Владимирович<br>477<br>${p}</h1>'); 
 }); 
 const PORT = 3000; 
 server.listen(PORT, () => { 
